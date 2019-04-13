3d77b690b46d92aeba0f738a7396ebc871efbabd
Jenny Chu <jchudesign0930@gmail.com>
2019-04-13 01:03:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
+require 'pry'
+require 'nokogiri'
+
 # require libraries/modules here
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+kickstarter = Nokogiri::HTML(html)
+  
 end
+binding.pry
+
+# projects: kickstarter.css("li.project.grid_4")
